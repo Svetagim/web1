7930fdf98c82d9732d127da3ab54f381d6d6cd90
svetagimpelson <svetagimpelson@se.shenkar.ac.il>
2018-06-07 05:04:20 +0300

    final

---
+++ includes/javascript.js
@@ -0,0 +1,35 @@
+
+window.onload=function(){
+    console.log(" I'm in");
+
+    document.getElementById("toExecute").onclick=function () {
+        console.log("clicked first");
+        var obj = document.getElementById( "messageAlert");
+        obj.style.display = "block";
+    }
+
+    document.getElementById("btnSubmitFirstPart").onclick=function () {
+        console.log("clicked second");
+        var objFirst = document.getElementById( "messageAlert");
+        objFirst.style.display = "none";
+        var objSecond = document.getElementById( "messageInput");
+        objSecond.style.display = "block";
+    }
+
+    document.getElementById("cancel1").onclick=function () {
+        console.log("clicked first");
+        var obj = document.getElementById( "messageAlert");
+        obj.style.display = "none";
+    }
+
+
+
+    document.getElementById("cancel2").onclick=function () {
+        console.log("clicked second");
+        var objFirst = document.getElementById( "messageAlert");
+        objFirst.style.display = "block";
+        var objSecond = document.getElementById( "messageInput");
+        objSecond.style.display = "none";
+    }
+
+}
